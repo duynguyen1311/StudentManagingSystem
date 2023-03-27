330240fc535dce0652df20f44b557195e9ce2938
duynguyen1311 <duynnhe163128@fpt.edu.vn>
2023-03-27 17:47:08 +0700

    update

--- StudentManagingSystem/StudentManagingSystem/Pages/Index.cshtml.cs
+++ StudentManagingSystem/StudentManagingSystem/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using StudentManagingSystem.Model;
+using StudentManagingSystem.Repository.IRepository;
 using StudentManagingSystem.Utility;
 
 namespace StudentManagingSystem.Pages
@@ -13,17 +14,26 @@ namespace StudentManagingSystem.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly UserManager<AppUser> _userManager;
-
-        public IndexModel(ILogger<IndexModel> logger, RoleManager<IdentityRole> roleManager,UserManager<AppUser> userManager)
+        private readonly INotiRepository _repository;
+        
+        public Notification Notification { get; set; }
+        public List<Notification> listNoti { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, RoleManager<IdentityRole> roleManager,UserManager<AppUser> userManager, INotiRepository repository)
         {
             _logger = logger;
             _roleManager = roleManager;
             _userManager = userManager;
+            _repository = repository;
         }
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
-            
+            listNoti = await _repository.GetAll();
+            return Page();
+        }
+        public async Task<IActionResult> OnGetDetail(Guid id)
+        {
+            Notification = await _repository.GetById(id);
             return Page();
         }
     }
